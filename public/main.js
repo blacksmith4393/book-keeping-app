acaf94cacb9d5f8e553b759e4fb41230e0a794c3
Zach <zach.smith33@gmail.com>
2017-03-02 23:38:54 -0500

    fix form input and Book creation. add mustache as dependency

--- public/main.js
+++ public/main.js
@@ -1,43 +1,45 @@
 var addBookButton = document.getElementById('addBookButton');
 var bookList = document.getElementById('bookList');
+var Mustache = require('mustache');
 
-var Book = {
-  title: '',
-  author: '',
-  rating: '',
-  createBook: function(arr){
-    let book = Object.create(Book);
-    console.log(arr);
-    return book;
-  }
+var Book = function(){
+  this.title = '';
+  this.author = '';
+  this.rating = '';
 };
 
 var bookLibrary = {
   books: [],
   getBookInfo: function(){
-    var bookTitle = document.getElementById('bookTitle').value;
-    var bookAuthor = document.getElementById('bookAuthor').value;
-    var bookRating =document.getElementById('bookRating').value;
-    var bookInfo = [bookTitle, bookAuthor, bookRating];
+    let bookTitle = document.getElementById('bookTitle').value;
+    let bookAuthor = document.getElementById('bookAuthor').value;
+    let bookRating =document.getElementById('bookRating').value;
+    let bookInfo = [bookTitle, bookAuthor, bookRating];
     return bookInfo;
   },
   addBook: function(){
-    bookInfo = this.getBookInfo();
-    var newBook = Book.createBook(bookInfo);
+    let bookInfo = this.getBookInfo();
+    let newBook = new Book();
+    Object.keys(newBook).forEach(function(key, index){
+      newBook[key] = bookInfo[index];
+    });
     this.books.unshift(newBook);
     this.renderBooks();
   },
   renderBooks: function(){
+    while (bookList.hasChildNodes()) {
+    bookList.removeChild(bookList.lastChild);
+    }
     this.books.forEach(function(book){
       //console.log(book);
       let li = document.createElement('li');
       li.className="list-group-item";
       let h4 = document.createElement('h4');
-      h4.innerHTML = book;
+      h4.innerHTML = book.title;
       li.appendChild(h4);
       bookList.appendChild(li);
     });
   }
 };
 
-addBookButton.addEventListener('click', bookLibrary.addBook);
+addBookButton.addEventListener('click', bookLibrary.addBook.bind(bookLibrary));
