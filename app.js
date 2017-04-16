1e36ff38959e9d303296e25c1a09bccaa7cb95c5
Zach <zach.smith33@gmail.com>
2017-04-15 21:34:28 -0400

    add new templating engine. refactor views directory.

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-
-
 const app = express();
+
+app.set('port', (process.env.PORT) || 5000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname,'public')));
 
-// app.get('/', function (req, res) {
-//   res.render('index');
-// });
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
 
-app.listen(3000, function(){
-  console.log('app listening on port 3000');
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
