916fe73e73d7242d0a1c5731505928dcc6b77743
Zach <zach.smith33@gmail.com>
2017-04-17 16:08:12 -0400

    add no-console rule to eslint config. refactor getBook.js

--- getBook.js
+++ getBook.js
@@ -1,18 +1,59 @@
 var https = require('https');
+var url = require('url');
+var qs = require('querystring');
 
-// https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
+var sampleHref = 'https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&'+ KEY;
+
+var KEY = process.env.KEY;
+
+var query = {};
+if(process.argv[2]){
+  query.intitle = process.argv[2];
+}
+if(process.argv[3]){
+  query.inauthor = process.argv[3];
+}
+if(process.argv[4]){
+  query.isbn = process.argv[4];
+}
+
+
+var searchQuery = qs.stringify(query,'+', ':');
+console.log(searchQuery);
 
 var options = {
+  protocol: 'https:',
+  slashes: true,
   hostname: 'www.googleapis.com',
-  path: '/books/v1/volumes',
-  query: 'q=flowers+inauthor:keyes',
-  method: 'GET',
-  'api-key': 'AIzaSyAv5_xYzI-SQX3byRz4wulFAVjLCk6CS6g'
-
+  search: '?q='+ searchQuery+ '&' + KEY,
+  pathname: '/books/v1/volumes'
 };
 
-https.get(options, function (response) {
-  response.setEncoding('utf8');
-  response.on('data', console.log);
-  response.on('error', console.error);
-}).on('error', console.error);
+var href = url.format(options);
+
+console.log(href);
+
+function callback(response) {
+  var data = [];
+  if(response.statusCode !== 200){
+    console.log(response.statusCode + ':' + response.statusMessage);
+  } else {
+    response.setEncoding('utf8');
+    response.on('data', (chunk) => {
+      data.push(chunk);
+    });
+    response.on('end', function() {
+      data = data.join('');
+      data = JSON.parse(data);
+      console.log(data.items[0].volumeInfo);
+    });
+    response.on('error', console.error);  
+  }
+}
+
+var request = https.get(href, callback);
+
+request.on('error', (e) => {
+  console.log(e.message);
+});
+request.end();
