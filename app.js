ab15266c17810335cf52ca71b09b1611927e65cf
Zach <zach.smith33@gmail.com>
2017-04-24 12:37:28 -0400

    configure database. set up connection in app.js.

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 
-// MODULES ============================================
+// MODULES ===============================================
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const getBooks = require('./getBooks.js');
+const getBooks = require('./getBooks');
 const users = require('./app/routes/users');
+const database = require('./config/database');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// CONNECT TO DATABASE ===================================
+mongoose.connect(database.url);
+mongoose.connection.on('connected', () => {
+  console.log('connected to database:' + database.url);
+});
+mongoose.connection.on('error', (err) => {
+  console.log('database error:' + err);
+});
 
 //APP CONFIGURATION ======================================
 
--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/books-app',
+  url: 'mongodb://localhost:27017/books-app',
   secret: 'secret'
 };
