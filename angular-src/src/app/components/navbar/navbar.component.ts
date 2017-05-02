d869dac370822f8ddafa434572b04ab3a9260f2d
Zach <zach.smith33@gmail.com>
2017-05-02 08:40:11 -0400

    fix collapse function of navbar component

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  isIn = false;   // store state
+  toggleState() { // click handler
+    let bool = this.isIn;
+    this.isIn = bool === false ? true : false; 
+  }
   constructor() { }
 
   ngOnInit() {
